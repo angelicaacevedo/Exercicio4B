cf7f8e3382336eecf952b603354094cd7c7a4f30
Angélica Acero Acevedo <aceroacevedoangelica@gmail.com>
2022-10-04 21:37:54 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            VerificaPares teste = new VerificaPares(10);
+
+             
+
+            while(!teste.terminou())
+            {
+                if (teste.par())
+                {
+                    Console.WriteLine("{0} é par", teste.valor);
+                }
+
+                teste.progride();
+            }
+
+            Console.WriteLine("FIM");
+        }
+    }
+}
---
+++ VerificaPares.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4B
+{
+    internal class VerificaPares
+    {
+        public int valor;
+
+        public VerificaPares(int numero)
+        {
+            valor = numero;
+        }
+
+        public bool par()
+        {
+            if (valor % 2 == 0) return true;
+            return false;
+        }
+
+        public bool terminou()
+        {
+            if(valor == 0) return true;
+
+            return false;
+        }
+
+        public void progride()
+        {
+            valor -= 1;        
+        }
+    }
+}
